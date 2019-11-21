3e6216335133d5953ecd98c8bb6f7054b95259cf
Sherwin <sherwin.lorico@gmail.com>
2019-11-21 20:18:21 +0800

    remove add button

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,9 @@
 export const environment = {
   production: false,
   firebaseConfig: {
+    // apiKey: 'AIzaSyDea27cd3oEF_h9oco_mWZUIH2L00kTF6s',
+    // authDomain: 'towa-test.firebaseapp.com',
+    // projectId: 'towa-test'
     apiKey: "AIzaSyD02Ou_3KYmOIyZat8jyDei1-1YRX-36UE",
     authDomain: "queueportal-51fd0.firebaseapp.com",
     databaseURL: "https://queueportal-51fd0.firebaseio.com",
@@ -12,21 +15,5 @@ export const environment = {
     storageBucket: "queueportal-51fd0.appspot.com",
     messagingSenderId: "108798178059",
     appId: "1:108798178059:web:36cf26f51e4797d1cfa80e"
-    // apiKey: "AIzaSyDea27cd3oEF_h9oco_mWZUIH2L00kTF6s",
-    // authDomain: "towa-test.firebaseapp.com",
-    // databaseURL: "YOUR_DATABASE_URL",
-    // projectId: "towa-test",
-    // storageBucket: "YOUR_STORAGE_BUCKET",
-    // messagingSenderId: "YOUR_MESSAGING_SENDER_ID"
   }
-
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
